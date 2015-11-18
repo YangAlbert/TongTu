392a5431cb773bae28d963ee3fcb5eaee3b46e70
Yang Ruijian <ruijian.yang@gmail.com>
2015-11-18 22:39:41 +0800

    revert compileSdkVersion to 23, fix gradle compile errors.

--- app/src/main/java/com/tongtu/www/tongtu/MainActivity.java
+++ app/src/main/java/com/tongtu/www/tongtu/MainActivity.java
@@ -2,7 +2,6 @@ package com.tongtu.www.tongtu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
