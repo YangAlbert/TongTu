8ff4c20162330789b88f8694e7c166d133e10390
Yang Ruijian <ruijian.yang@gmail.com>
2015-11-18 20:30:35 +0800

    modify app manifest file; fix library(.so) link problem;

--- app/src/main/java/com/tongtu/www/tongtu/MainActivity.java
+++ app/src/main/java/com/tongtu/www/tongtu/MainActivity.java
@@ -2,6 +2,7 @@ package com.tongtu.www.tongtu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
